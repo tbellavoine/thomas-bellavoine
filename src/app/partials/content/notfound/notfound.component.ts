0408fac2eca20fda1d2f934cc224aedb26d11e12
tbellavoine <tbellavoine@users.noreply.github.com>
2020-04-08 21:58:02 +0200

    componenent notfound

---
+++ src/app/partials/content/notfound/notfound.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'pfo-notfound',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.scss']
+})
+export class NotfoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
